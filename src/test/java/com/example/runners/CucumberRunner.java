577a0b5e5d19a334e21fdc1cdc11f65039f0b2eb
memintan <memintan@gmail.com>
2021-04-13 17:31:46 -0500

    Runner Class and step definitions are created

--- src/test/java/com/example/runners/CucumberRunner.java
+++ src/test/java/com/example/runners/CucumberRunner.java
@@ -1,4 +1,17 @@
 package com.example.runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com/example/step_definitions",
+        dryRun = true,
+        tags = "@api"
+
+)
+
 public class CucumberRunner {
 }
